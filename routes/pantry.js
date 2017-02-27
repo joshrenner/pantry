483a33942b785f9fb6d4e9964c03c80a3ebdd2d1
Josh Renner <josh@renner.mn>
2017-02-27 16:46:26 -0600

    adding ingredients

--- routes/pantry.js
+++ routes/pantry.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var ingredients = require('../data/ingredients.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pantry', author:'author2' });
+  res.render('pantry', { title: 'Pantry', ingredients: ingredients });
 });
 
 module.exports = router;
